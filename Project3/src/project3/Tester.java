d0a5da756fed39ef2d8236717c0844411d368516
Dahamonnah <dahamonnah@hotmail.com>
2016-12-07 00:48:17 +0300

    1

---
+++ Project3/src/project3/Tester.java
@@ -0,0 +1,10 @@
+
+package project3;
+
+public class Tester {
+    public static void main(String[] args){
+        CCNode node = new CCNode(4507112832632843l, 0.0, 0.0);
+        CCLogger logger = new CCLogger();
+        System.out.println(String.format("%s", node.getCCNumber() + ""));
+    }
+}
