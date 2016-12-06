5517652799840606e82d6ca4775721e13fddf519
Dahamonnah <dahamonnah@hotmail.com>
2016-12-07 00:48:10 +0300

    1

---
+++ Project3/src/project3/CCLogger.java
@@ -0,0 +1,106 @@
+
+package project3;
+
+public class CCLogger {
+    
+    private CCNode[] Hashtable;
+    private int size;
+
+    public CCLogger() {
+        Hashtable = new CCNode[10];
+        setSize(0);
+    }
+
+    public CCNode[] getHashtable() {
+        return Hashtable;
+    }
+
+    public void setHashtable(CCNode[] Hashtable) {
+        this.Hashtable = Hashtable;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    public int getHash(long Key){
+        String k = Key + "";
+        return (Integer.parseInt(k.charAt(3) + "") + Integer.parseInt(k.charAt(7) + "")
+                + Integer.parseInt(k.charAt(11) + "") + Integer.parseInt(k.charAt(15) + "")) % 10;
+    }
+    
+    public void Add(CCNode node){
+        int hash = getHash(node.getCCNumber());
+        
+        if(search(node.getCCNumber()) == null){
+            System.out.println("Cannot add duplicates");
+        }
+        else{
+            if(Hashtable[hash] == null){
+                Hashtable[hash] = node;
+            }
+            else{
+                node.setNext(Hashtable[hash]);
+                Hashtable[hash] = node;
+            }
+
+            size++;
+            log(node);
+        }
+    }
+    
+    public void Credit(long Key, double Amt){
+        CCNode temp = search(Key);
+        
+        if(Amt < temp.getBalance()){
+            System.out.println("Unable to complete operation");
+        }
+        else{
+            temp.setBalance(temp.getBalance() - Amt);
+        }
+        
+        log(temp);
+    }
+    
+    public void Debit(long Key, double Amt){
+        CCNode temp = search(Key);
+        
+        temp.setBalance(temp.getBalance() + Amt);
+        if(temp.getBalance() > temp.getCLimit()){
+            System.out.println("Warning. Credit limit exceeded");
+        }
+        
+        log(temp);
+    }
+    
+    public void log(CCNode node){
+        String log = String.format("Current_Time %s\tCCNumber %s\tCLimit %f\tBalance %f\n", 
+                (System.currentTimeMillis() / 1000) + "", node.getCCNumber() + "", 
+                node.getCLimit(), node.getBalance());
+        node.setLog(node.getLog() + log);
+    }
+    
+    public String getLog(long Key){
+        return search(Key).getLog();
+    }
+    
+    private CCNode search(long Key){
+        int hash = getHash(Key);
+        CCNode temp = Hashtable[hash];
+        
+        while(temp != null){
+            if(Key == temp.getCCNumber()){
+                break;
+            }
+            temp = temp.getNext();
+        }
+        
+        return temp;
+        
+    }
+    
+}
