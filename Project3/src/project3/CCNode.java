21112c352d452f2ed80b74f8407e0cd89acb1eee
Dahamonnah <dahamonnah@hotmail.com>
2016-12-07 00:47:29 +0300

    1

--- Project3/src/project3/CCNode.java
+++ Project3/src/project3/CCNode.java
@@ -4,6 +4,59 @@ package project3;
 public class CCNode {
     
     private long CCNumber;
+    private double CLimit;
+    private double Balance;
+    private CCNode Next;
+    private String Log;
+
+    public CCNode(long CCNumber, double CLimit, double Balance) {
+        setCCNumber(CCNumber);
+        setCLimit(CLimit);
+        setBalance(Balance);
+        setNext(null);
+    }
+
+    public long getCCNumber() {
+        return CCNumber;
+    }
+
+    public void setCCNumber(long CCNumber)  {
+        if((CCNumber + "").length() == 16){
+            this.CCNumber = CCNumber;
+        }
+    }
+
+    public double getCLimit() {
+        return CLimit;
+    }
+
+    public void setCLimit(double CLimit) {
+        this.CLimit = CLimit;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(double Balance) {
+        this.Balance = Balance;
+    }
+
+    public CCNode getNext() {
+        return Next;
+    }
+
+    public void setNext(CCNode next) {
+        this.Next = next;
+    }
+
+    public String getLog() {
+        return Log;
+    }
+
+    public void setLog(String Log) {
+        this.Log = Log;
+    }
     
     
 }
